16a834aa48a58c75db52c0a169249e655c3cc9dc
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2024-01-15 16:32:43 +0300

    Create 05_2_Tuples_Sets_Dictionaries.py

---
+++ 05_2_Tuples_Sets_Dictionaries.py
@@ -0,0 +1,9 @@
+
+##### Python 3.12.1 tutorial from the link: https://www.python.org/ --> Docs #####
+##### 01/11/2024 #####
+
+##### tested on _________  -  Microsoft Windows - Python 3.12.0 (Oct  2 2023) #####
+
+
+##### 5.3. Tuples and Sequences #####
+
