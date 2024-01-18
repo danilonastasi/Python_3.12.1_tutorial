33777cc3ffd0704caff45a3d805273439d32b863
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2024-01-18 13:54:57 +0300

    Update 12_VirtualEnv_Pip_Packages.py

--- 12_VirtualEnv_Pip_Packages.py
+++ 12_VirtualEnv_Pip_Packages.py
@@ -2,7 +2,7 @@
 ##### Python 3.12.1 tutorial from the link: https://www.python.org/ --> Docs #####
 ##### 01/11/2024 #####
 
-##### tested on ________ -  Microsoft Windows - Python 3.12.0 (Oct  2 2023) #####
+##### tested on 01/18/2024 -  Microsoft Windows - Python 3.12.0 (Oct  2 2023) #####
 
 
 ##### 12. Virtual Environments and Packages #####
@@ -64,7 +64,7 @@ tutorial-env\Scripts\activate
 
 # On Unix or MacOS, run:
 
-source tutorial-env/bin/activate
+# source tutorial-env/bin/activate
 
 # (This script is written for the bash shell. If you use the csh or fish shells, 
 # there are alternate activate.csh and activate.fish scripts you should use instead.)
@@ -180,4 +180,107 @@ deactivate
     # Creation of virtual environments is done through the venv module. 
     # Installing packages into an active virtual environment uses the commands 
     # shown above.
+
+
+##### 12.3. Managing Packages with pip - continue #####
+
+# You can install the latest version of a package by specifying a package’s name:
+
+# (tutorial-env) $ 
+python -m pip install novas
+# Collecting novas
+#   Downloading novas-3.1.1.3.tar.gz (136kB)
+# Installing collected packages: novas
+#   Running setup.py install for novas
+# Successfully installed novas-3.1.1.3
+
+# You can also install a specific version of a package by giving the package name 
+# followed by == and the version number:
+
+# (tutorial-env) $ 
+python -m pip install requests==2.6.0
+# Collecting requests==2.6.0
+#   Using cached requests-2.6.0-py2.py3-none-any.whl
+# Installing collected packages: requests
+# Successfully installed requests-2.6.0
+
+# If you re-run this command, pip will notice that the requested version is 
+# already installed and do nothing. You can supply a different version number 
+# to get that version, or you can run python -m pip install --upgrade to upgrade 
+# the package to the latest version:
+
+# (tutorial-env) $ 
+python -m pip install --upgrade requests
+# Collecting requests
+# Installing collected packages: requests
+#   Found existing installation: requests 2.6.0
+#     Uninstalling requests-2.6.0:
+#       Successfully uninstalled requests-2.6.0
+# Successfully installed requests-2.7.0
+
+python -m pip uninstall PackageName
+# followed by one or more package names will remove the packages from the 
+# virtual environment.
+
+python -m pip show PackageName
+# will display information about a particular package:
+
+# (tutorial-env) $ 
+python -m pip show requests
+# ---
+# Metadata-Version: 2.0
+# Name: requests
+# Version: 2.7.0
+# Summary: Python HTTP for Humans.
+# Home-page: http://python-requests.org
+# Author: Kenneth Reitz
+# Author-email: me@kennethreitz.com
+# License: Apache 2.0
+# Location: /Users/akuchling/envs/tutorial-env/lib/python3.4/site-packages
+# Requires:
+
+python -m pip list  
+# will display all of the packages installed in the virtual environment:
+
+# (tutorial-env) $ 
+python -m pip list
+# novas (3.1.1.3)
+# numpy (1.9.2)
+# pip (7.0.3)
+# requests (2.7.0)
+# setuptools (16.0)
+
+python -m pip freeze 
+# will produce a similar list of the installed packages, but the output uses the 
+# format that python -m pip install expects. A common convention is to put this 
+# list in a requirements.txt file:
+
+# (tutorial-env) $ 
+python -m pip freeze > requirements.txt
+#(tutorial-env) $ 
+cat requirements.txt  # error because cat is a Unix command and not Windows
+# novas==3.1.1.3
+# numpy==1.9.2
+# requests==2.7.0
+
+# The requirements.txt can then be committed to version control and shipped as 
+# part of an application. Users can then install all the necessary packages with 
+# install -r:
+
+# (tutorial-env) $ 
+python -m pip install -r requirements.txt
+# Collecting novas==3.1.1.3 (from -r requirements.txt (line 1))
+#   ...
+# Collecting numpy==1.9.2 (from -r requirements.txt (line 2))
+#   ...
+# Collecting requests==2.7.0 (from -r requirements.txt (line 3))
+#   ...
+# Installing collected packages: novas, numpy, requests
+#   Running setup.py install for novas
+# Successfully installed novas-3.1.1.3 numpy-1.9.2 requests-2.7.0
+
+
+
+
+
 
